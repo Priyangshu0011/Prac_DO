460ecc63c7818eb2c573e5f38c721bd505505576
Priyangshu Sett <149405279+Priyangshu0011@users.noreply.github.com>
2025-11-12 00:06:20 +0530

    Update Echo.cpp

--- Echo.cpp
+++ Echo.cpp
@@ -5,9 +5,4 @@ int main()
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "Yo!!" << std::endl;
 	return 0;
-=======
-int main() {
-ok
-return 0;
->>>>>>> 5d3e3d35510385359aa3b5a346627b3c9775949c
 }
